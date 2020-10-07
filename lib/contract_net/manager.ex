6300c16831013496dbf9301912055ba731f76eb9
antedeguemon <antedeguemon@users.noreply.github.com>
2020-10-06 23:41:29 -0300

    Add received_all_proposals?/2

--- lib/contract_net/manager.ex
+++ lib/contract_net/manager.ex
@@ -18,4 +18,8 @@ defmodule ContractNet.Manager do
     |> Map.put(:offers_sent, [])
     |> Map.put(:proposals_received, [])
   end
+
+  defp received_all_proposals?(proposals, state) do
+    length(proposals) == length(state.offers_sent)
+  end
 end
